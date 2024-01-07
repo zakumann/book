6cd0be79a292f6fc9d485451a9e7b30584ab2050
zakumann <samuel742able@gmail.com>
2024-01-07 20:50:22 +0900

    first full stack

---
+++ client/src/pages/Add.jsx
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+const Add = () => {
+    const [book, setBook] = useState({
+        title:'',
+        desc:'',
+        price:null,
+        cover:'',
+    });
+
+    const navigate = useNavigate()
+
+    const handleChange = (e) => {
+        setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    };
+
+    const handleClick = async e => {
+        e.preventDefault()
+        try{
+            await axios.post("http://localhost:8080/books", book)
+            navigate("/")
+        }catch (err){
+            console.log(err)
+        }
+    }
+
+  return (
+    <div className='form'>
+        <h1>Add New Book</h1>
+        <input type="text" placeholder='title' onChange={handleChange} name='title'/>
+        <input type="text" placeholder='desc' onChange={handleChange} name='desc'/>
+        <input type="number" placeholder='price' onChange={handleChange} name='price'/>
+        <input type="text" placeholder='cover' onChange={handleChange} name='cover'/>
+        <button className="formButton" onClick={handleClick}>Add</button>
+    </div>
+  )
+}
+
+export default Add
